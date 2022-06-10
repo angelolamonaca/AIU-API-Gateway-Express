af4040c187ccf95d6bb8289d79c498b1be3d7925
Angelo Lamonaca <angelo.lamonaca@gmail.com>
2022-06-10 15:43:57 +0200

    Use proxy on auth

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const httpProxy = require('express-http-proxy')
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
 const app = express();
+const authServiceProxy = httpProxy('http://localhost:3020')
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,23 +21,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+  authServiceProxy(req, res, next)
+  next(createError(401));
+})
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 module.exports = app;
